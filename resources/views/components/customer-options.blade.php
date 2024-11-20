4a48b9c2fcbabae322a62da961fe17981fb7576a
MKashanButt <kashanbutt566@gmail.com>
2024-11-20 21:59:34 +0500

    updated customer payment button url and class

--- resources/views/components/customer-options.blade.php
+++ resources/views/components/customer-options.blade.php
@@ -15,7 +15,7 @@
             </button>
         </form>
         <a href="/add-customer-account"><button class="customer-account">Add Customer Account</button></a>
-        <a href="/add-customer-payments"><button class="customer-payments">Add Customer Payments</button></a>
+        <a href="/add-customer-payment"><button class="customer-payment">Add Customer Payment</button></a>
         <a href="/add-customer-vehicle"><button class="customer-vehicle">Add Customer Vehicle</button></a>
     </div>
 </div>
