591c04bed97c90f937dc466f408c0febe5f8b0eb
MKashanButt <kashanbutt566@gmail.com>
2024-11-21 02:15:05 +0500

    added isset on form fields

--- resources/views/pages/vehicle-docs/add-docs.blade.php
+++ resources/views/pages/vehicle-docs/add-docs.blade.php
@@ -18,36 +18,38 @@
                 <label for="stockid">Stock Id</label>
                 <input type="text" name="stock_id" id="stockid" value="{{ $id }}" readonly>
             </div>
-            @if (!$documents->japanese_export)
-                <div class="field">
-                    <label for="japanese_export">Upload Japanese Export</label>
-                    <input type="file" name="japanese_export" id="japanese_export">
-                </div>
-            @endif
-            @if (!$documents->english_export)
-                <div class="field">
-                    <label for="english_export">Upload English Export</label>
-                    <input type="file" name="english_export" id="english_export">
-                </div>
-            @endif
-            @if (!$documents->final_invoice)
-                <div class="field">
-                    <label for="final_invoice">Upload Final Invoice</label>
-                    <input type="file" name="final_invoice" id="final_invoice">
-                </div>
-            @endif
-            @if (!$documents->inspection_certificate)
-                <div class="field">
-                    <label for="inspection_certificate">Upload Inspection Certificate</label>
-                    <input type="file" name="inspection_certificate" id="inspection_certificate">
-                </div>
-            @endif
-            @if (!$documents->bl_copy)
-                <div class="field">
-                    <label for="bl_copy">Upload BL Copy</label>
-                    <input type="file" name="bl_copy" id="bl_copy">
-                </div>
-            @endif
+            @isset($documents)
+                @if (!$documents->japanese_export)
+                    <div class="field">
+                        <label for="japanese_export">Upload Japanese Export</label>
+                        <input type="file" name="japanese_export" id="japanese_export">
+                    </div>
+                @endif
+                @if (!$documents->english_export)
+                    <div class="field">
+                        <label for="english_export">Upload English Export</label>
+                        <input type="file" name="english_export" id="english_export">
+                    </div>
+                @endif
+                @if (!$documents->final_invoice)
+                    <div class="field">
+                        <label for="final_invoice">Upload Final Invoice</label>
+                        <input type="file" name="final_invoice" id="final_invoice">
+                    </div>
+                @endif
+                @if (!$documents->inspection_certificate)
+                    <div class="field">
+                        <label for="inspection_certificate">Upload Inspection Certificate</label>
+                        <input type="file" name="inspection_certificate" id="inspection_certificate">
+                    </div>
+                @endif
+                @if (!$documents->bl_copy)
+                    <div class="field">
+                        <label for="bl_copy">Upload BL Copy</label>
+                        <input type="file" name="bl_copy" id="bl_copy">
+                    </div>
+                @endif
+            @endisset
             <button class="primary">Add</button>
             @if (
                 $documents->japanese_export &&
