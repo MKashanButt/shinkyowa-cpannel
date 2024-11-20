470ba005a6ee4b3aae928b07a6ea0740577a466d
MKashanButt <kashanbutt566@gmail.com>
2024-11-21 01:58:01 +0500

    added fillables for table columns

--- app/Models/Docs.php
+++ app/Models/Docs.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Docs extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'stock_id',
+        'japanese_export',
+        'english_export',
+        'final_invoice',
+        'inspection_certificate',
+        'bl_copy'
+    ];
 }
