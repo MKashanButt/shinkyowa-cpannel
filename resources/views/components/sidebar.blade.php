90b089f1f77080c364b05b737fbeeab710fc920e
MKashanButt <kashanbutt566@gmail.com>
2024-12-05 02:14:58 +0500

    added 2 new pages and sub-pages - Recently Added TT's - TT's

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -11,11 +11,30 @@
             <a href="/customer-account">
                 <li>Customer Accounts</li>
             </a>
-            {{-- @if (Auth::user()->role == 'admin')
+            @if (Auth::user()->role == 'admin')
                 <a href="/recently-added-tts">
-                    <li>Recently Added TT's</li>
+                    <li class="{{ Auth::user()->hasUnviewedTTs() ? 'tt-added' : '' }}">Recently Added TT's
+                        @if (Auth::user()->hasUnviewedTTs())
+                            <span class="alert">{{ Auth::user()->unviewedTTCount() }}</span>
+                        @endif
+                    </li>
                 </a>
-            @endif --}}
+            @endif
+            @if (Auth::user()->role == 'agent' || Auth::user()->role == 'manager')
+                <div x-data='{open: false}' x-cloak>
+                    <li @click='open=!open' @click.outside='open=false'>TTs</li>
+                    <div class="sub-menu" x-show="open" x-transition>
+                        <ul>
+                            <a href="/tt/pending-tts">
+                                <li>Pending TT's</li>
+                            </a>
+                            <a href="/tt/add-tt">
+                                <li>Add New TT</li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            @endif
             @if (Auth::user()->role != 'agent')
                 <a href="/users">
                     <li>Users</li>
