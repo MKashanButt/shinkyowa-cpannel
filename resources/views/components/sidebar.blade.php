725245b8743cd74377ea30062f706434e80f1e30
MKashanButt <kashanbutt566@gmail.com>
2024-12-10 22:21:51 +0500

    redesigned slider and added collapsable feature

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,45 +1,164 @@
-<aside>
-    <img src="/images/logo.png" alt="">
-    <nav>
-        <ul>
-            <a href="/">
-                <li>Dashboard</li>
-            </a>
-            {{-- <a href="/customer-account">
-                <li>Company Accounts</li>
-            </a> --}}
-            <a href="/customer-account">
-                <li>Customer Accounts</li>
-            </a>
-            @if (Auth::user()->role == 'admin')
-                <a href="/recently-added-tts">
-                    <li class="{{ Auth::user()->hasUnviewedTTs() ? 'tt-added' : '' }}">Recently Added TT's
-                        @if (Auth::user()->hasUnviewedTTs())
-                            <span class="alert">{{ Auth::user()->unviewedTTCount() }}</span>
-                        @endif
+<section x-data="{
+    closed: true,
+    closedIcon: `<svg xmlns='http: //www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' class='icon'>
+        <path stroke - linecap = 'round' stroke-linejoin = 'round' d = 'M3.75 9h16.5m-16.5 6.75h16.5' / >
+    </svg>`,
+    openIcon: `<svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5'
+    stroke='currentColor' class='icon'>
+    <path stroke-linecap='round' stroke-linejoin='round' d='M6 18 18 6M6 6l12 12' />
+    </svg>
+    `
+}" x-cloak x-bind:class="closed ? 'sidabar-closed' : 'sidebar-alpine-container'">
+    <aside x-bind:class="closed ? 'closed' : ''">
+        <img src="/images/logo.png" alt="">
+        <hr>
+        <nav>
+            <ul>
+                <a href="/">
+                    <li class="{{ Request::is('/') ? 'active-url' : '' }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
+                        </svg>
+                        Dashboard
                     </li>
                 </a>
-            @endif
-            {{-- @if (Auth::user()->role == 'agent' || Auth::user()->role == 'manager') --}}
-            <div x-data='{open: false}' x-cloak>
-                <li @click='open=!open' @click.outside='open=false'>TTs</li>
-                <div class="sub-menu" x-show="open" x-transition>
-                    <ul>
-                        <a href="/tt/pending-tts">
-                            <li>Pending TT's</li>
-                        </a>
-                        <a href="/tt/add-tt">
-                            <li>Add New TT</li>
-                        </a>
-                    </ul>
-                </div>
-            </div>
-            {{-- @endif --}}
-            @if (Auth::user()->role != 'agent')
-                <a href="/users">
-                    <li>Users</li>
+                {{-- <a href="/customer-account">
+                <li>Company Accounts</li>
+            </a> --}}
+                <a href="/customer-account">
+                    <li class="{{ Request::is('customer-account') ? 'active-url' : '' }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                        </svg>
+                        Customer Accounts
+                    </li>
                 </a>
-            @endif
-        </ul>
-    </nav>
-</aside>
+                @if (Auth::user()->role == 'admin')
+                    <a href="/recently-added-tts">
+                        <li class="{{ Auth::user()->hasUnviewedTTs() ? 'tt-added' : '' }}"
+                            class="{{ Request::is('/') ? 'active-url' : '' }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="icon">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+                            </svg>
+                            Recently Added TT's
+                            @if (Auth::user()->hasUnviewedTTs())
+                                <span class="alert">{{ Auth::user()->unviewedTTCount() }}</span>
+                            @endif
+                        </li>
+                    </a>
+                @endif
+                {{-- @if (Auth::user()->role == 'agent' || Auth::user()->role == 'manager') --}}
+                <div x-data='{open: false, active: false}' x-cloak>
+                    <li @click="open = !open; active = !active" @click.outside="open = false; active = false"
+                        x-bind:class="active ? 'active-sub-menu' : ''">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
+                        </svg>
+                        TTs
+                    </li>
+                    <div class="sub-menu" x-show="open" x-transition>
+                        <ul>
+                            <a href="/tt/pending-tts">
+                                <li class="{{ Request::is('/pending-tts') ? 'active-url' : '' }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="icon">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                    Pending TT's
+                                </li>
+                            </a>
+                            <a href="/tt/add-tt">
+                                <li class="{{ Request::is('/add-tt') ? 'active-url' : '' }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="icon">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                    Add New TT
+                                </li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+                {{-- @endif --}}
+                @if (Auth::user()->role != 'agent')
+                    <a href="/users">
+                        <li class="{{ Request::is('users') ? 'active-url' : '' }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="icon">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                            Users
+                        </li>
+                    </a>
+                @endif
+            </ul>
+        </nav>
+    </aside>
+
+    <aside x-bind:class="closed ? '' : 'closed'">
+        <img src="/images/logo-small.png" alt="">
+        <hr>
+        <nav>
+            <ul>
+                <li class="{{ Request::is('/') ? 'active-url' : '' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
+                    </svg>
+                </li>
+                {{-- <a href="/customer-account">
+                <li>Company Accounts</li>
+            </a> --}}
+                <li class="{{ Request::is('customer-account') ? 'active-url' : '' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                    </svg>
+                </li>
+                @if (Auth::user()->role == 'admin')
+                    <li class="{{ Auth::user()->hasUnviewedTTs() ? 'tt-added' : '' }}"
+                        class="{{ Request::is('/') ? 'active-url' : '' }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+                        </svg>
+                    </li>
+                @endif
+                {{-- @if (Auth::user()->role == 'agent' || Auth::user()->role == 'manager') --}}
+                <li @click="open = !open; active = !active" @click.outside="open = false; active = false"
+                    x-bind:class="active ? 'active-sub-menu' : ''">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 0 0-1.883 2.542l.857 6a2.25 2.25 0 0 0 2.227 1.932H19.05a2.25 2.25 0 0 0 2.227-1.932l.857-6a2.25 2.25 0 0 0-1.883-2.542m-16.5 0V6A2.25 2.25 0 0 1 6 3.75h3.879a1.5 1.5 0 0 1 1.06.44l2.122 2.12a1.5 1.5 0 0 0 1.06.44H18A2.25 2.25 0 0 1 20.25 9v.776" />
+                    </svg>
+                </li>
+                {{-- @endif --}}
+                <li class="{{ Request::is('users') ? 'active-url' : '' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="icon">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+                </li>
+                </a>
+            </ul>
+        </nav>
+    </aside>
+    <button class="toggle-sidebar-btn" @click="closed=!closed" x-html="closed ? closedIcon : openIcon">
+    </button>
+</section>
