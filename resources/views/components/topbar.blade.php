beb578c04d7fa9803dfd36ff2c47f0dcefab88a5
MKashanButt <kashanbutt566@gmail.com>
2024-12-11 00:55:43 +0500

    minor improvements: - removed js function used and replaced with alpine - added icons on account sub-menu

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -3,9 +3,9 @@
         User Role: {{ ucwords(Auth::user()->role) }}
     </button>
     <div class="options">
-        <div class="account">
+        <div class="account" x-data="{ show: false }">
             <div class="user">
-                <button onclick="toggleSubMenu()">
+                <button @click="show=!show">
                     <div>{{ Auth::user()->name }}</div>
                     <div class="icon">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -15,19 +15,35 @@
                     </div>
                 </button>
             </div>
-            <div class="options" id="topbar_submenu" style="display: none">
-                <x-dropdown-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-dropdown-link>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <x-dropdown-link :href="route('logout')"
-                        onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-dropdown-link>
-                </form>
-
+            <div class="options" id="topbar_submenu" x-show="show" @click.outside="show=false" x-cloak>
+                <ul>
+                    <li>
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="icon">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                    </li>
+                    <hr>
+                    <li>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <x-dropdown-link :href="route('logout')"
+                                onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="icon" style="transform: rotate(180deg)">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                        </svg>
+                    </li>
+                </ul>
             </div>
         </div>
         {{-- <div class="hidden sm:flex sm:items-center sm:ms-6">
