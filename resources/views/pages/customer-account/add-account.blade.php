0432901dd31697053d3da69d1e6d003985f1f0b3
Kashan Butt <kashanbutt566@gmail.com>
2024-12-12 07:10:03 +0500

    moved breadcrumbs inside the container

--- resources/views/pages/customer-account/add-account.blade.php
+++ resources/views/pages/customer-account/add-account.blade.php
@@ -1,8 +1,8 @@
 @extends('template')
 @section('content')
-    <x-breadcrumbs :page="'Sale'" :subpage="'Customer Accounts'" :category="'Add Customer'" />
     <div class="container">
-        <h2>Add Customer Accounts:</h2>
+        <x-breadcrumbs :page="'Sale'" :subpage="'Customer Accounts'" :category="'Add Customer'" />
+        <h2>Add Customer Accounts</h2>
         <form action="{{ $actionUrl }}" method="post">
             @csrf
             <div class="item">
@@ -59,17 +59,23 @@
             </div>
             <div class="item" style="align-items:baseline;">
                 <label for="cdescription">Description:</label>
-                <textarea name="cdescription" id="cdescription" cols="30" rows="2">{{ isset($customerAccount) ? $customerAccount->description : '' }}</textarea>
+                <textarea name="cdescription" id="cdescription" cols="30" rows="4">{{ isset($customerAccount) ? $customerAccount->description : '' }}</textarea>
             </div>
             <div class="item" style="align-items:baseline;">
                 <label for="caddress">Address:</label>
-                <textarea name="caddress" id="caddress" cols="30" rows="2">{{ isset($customerAccount) ? $customerAccount->address : '' }}</textarea>
+                <textarea name="caddress" id="caddress" cols="30" rows="4">{{ isset($customerAccount) ? $customerAccount->address : '' }}</textarea>
             </div>
             <div class="item">
                 <label for="cdescription">Agent:</label>
                 <input type="text" name="agent" id="agent" value="{{ Auth::user()->name }}" readonly>
             </div>
-            <button>Add</button>
+            <button>
+                {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="icon">
+                    <path stroke-linecap="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg> --}}
+                Add
+            </button>
         </form>
     </div>
 @endsection
