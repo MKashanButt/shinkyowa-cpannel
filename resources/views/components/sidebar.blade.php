83b041dac12a0a3883442043a17eb1fe0945b7ee
MKashanButt <kashanbutt566@gmail.com>
2025-01-23 20:16:01 +0500

    added stock option

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -134,6 +134,22 @@
                         </div>
                     </div>
                 @endif
+                @if (Auth::user()->role == 'manager' || Auth::user()->role == 'admin')
+                    <a href="/stocks">
+                        <li class="{{ Request::is('stocks') ? 'active-url' : '' }}" @click="closed=false">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" class="icon">
+                                <path
+                                    d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
+                                <circle cx="7" cy="17" r="2" />
+                                <path d="M9 17h6" />
+                                <circle cx="17" cy="17" r="2" />
+                            </svg>
+                            Stocks
+                        </li>
+                    </a>
+                @endif
                 @if (Auth::user()->role != 'agent' && Auth::user()->role != 'doc_uploader')
                     <a href="/users">
                         <li class="{{ Request::is('users') ? 'active-url' : '' }}">
@@ -205,6 +221,19 @@
                         </svg>
                     </li>
                 @endif
+                @if (Auth::user()->role == 'manager' || Auth::user()->role == 'admin')
+                    <li class="{{ Request::is('stocks') ? 'active-url' : '' }}" @click="closed=false">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="icon">
+                            <path
+                                d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
+                            <circle cx="7" cy="17" r="2" />
+                            <path d="M9 17h6" />
+                            <circle cx="17" cy="17" r="2" />
+                        </svg>
+                    </li>
+                @endif
                 @if (Auth::user()->role != 'agent' && Auth::user()->role != 'doc_uploader')
                     <li class="{{ Request::is('users') ? 'active-url' : '' }}" @click="closed=false">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -214,7 +243,6 @@
                         </svg>
                     </li>
                 @endif
-                </a>
             </ul>
         </nav>
     </aside>
