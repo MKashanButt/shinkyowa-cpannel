3c90610942c93caeb630a186ae0b2a1d90c026f0
MKashanButt <kashanbutt566@gmail.com>
2025-01-23 20:16:46 +0500

    added fillables

--- app/Models/Stocks.php
+++ app/Models/Stocks.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Stocks extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'thumbnail',
+        'stock_id',
+        'chassis',
+        'make',
+        'model',
+        'year',
+        'fob',
+        'currency',
+        'mileage',
+        'doors',
+        'transmission',
+        'body_type',
+        'fuel',
+        'category',
+        'country',
+        'features',
+        'thumbnail',
+        'stock_images'
+    ];
+
+    protected $casts = [
+        'stock_images' => 'array',
+    ];
 }
