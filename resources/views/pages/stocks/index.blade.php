5a0e5af764e659807919cbc5686527b9a7f7da47
MKashanButt <kashanbutt566@gmail.com>
2025-01-23 20:18:11 +0500

    added stocks page and styling

---
+++ resources/views/pages/stocks/index.blade.php
@@ -0,0 +1,89 @@
+@php
+    $count = 1;
+@endphp
+
+@extends('template')
+@section('content')
+    <section class="stocks">
+        <x-breadcrumbs :page="'Stocks'" :subpage="'All Stock'" />
+        @if (session('success'))
+            <div class="alert" style="margin: 20px 0">
+                <p>{{ session('success') }}</p>
+            </div>
+        @endif
+        <a href="/stocks/add">
+            <button class="header">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round" class="icon">
+                    <path
+                        d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
+                    <circle cx="7" cy="17" r="2" />
+                    <path d="M9 17h6" />
+                    <circle cx="17" cy="17" r="2" />
+                </svg>
+                Add Vehicle
+            </button>
+        </a>
+        <div class="container">
+            <table cellspacing="0">
+                <thead>
+                    <tr>
+                        <th class="sno">S.No</th>
+                        <th class="image">Image</th>
+                        <th class="stock_id">Stock Id</th>
+                        <th class="chassis">Chassis</th>
+                        <th class="make_model">Make/Model</th>
+                        <th class="year">Year</th>
+                        <th class="body">Body</th>
+                        <th class="fob">FOB</th>
+                        <th class="status">Status</th>
+                        <th class="actions">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($data as $item)
+                        <tr>
+                            @if ($count < 10)
+                                <td>0{{ $count++ }}.</td>
+                            @else
+                                <td>{{ $count++ }}.</td>
+                            @endif
+                            <td><img src="{{ $item['thumbnail'] }}" alt=""
+                                    onerror="this.src='https://placehold.co/200x100'"></td>
+                            <td>{{ $item['stock_id'] }}</td>
+                            <td>{{ $item['chassis'] }}</td>
+                            <td>{{ $item['make'] . '/' . $item['model'] }}</td>
+                            <td>{{ $item['year'] }}</td>
+                            <td>{{ $item['body_type'] }}</td>
+                            <td>{{ $item['currency'] . $item['fob'] }}</td>
+                            <td>
+                                <button
+                                    class="{{ $item['status'] == 'reserved' ? 'reserved danger' : 'primary' }}">{{ $item['status'] }}</button>
+                            </td>
+                            <td class="actions">
+                                <div class="stage">
+                                    <a href="https://shinkyowa.com/vehicle-info/{{ $item['id'] }}" target="__blank">
+                                        <button class="done">View</button>
+                                    </a>
+                                    <a href="/stocks/edit/{{ $item['id'] }}">
+                                        <button class="primary">Edit</button>
+                                    </a>
+                                    <a href="/{{ $item['id'] }}">
+                                        <button class="danger">Delete</button>
+                                    </a>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="10">
+                            {{ $data->links('components.pagination') }}
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/pages/stocks/stock-form.blade.php
@@ -0,0 +1,197 @@
+@extends('template')
+@section('content')
+    <div class="container">
+        <x-breadcrumbs :page="'Stocks'" :subpage="'All Stock'" :category="$category" />
+        @isset($success)
+            <div class="success">
+                <p>{{ $success }}</p>
+            </div>
+        @endisset
+        <h2>{{ $formTitle }}</h2>
+        <form action="{{ $actionUrl }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="item">
+                <label for="stock_id">Stock Id:</label>
+                <input type="text" id="stock_id" name="stock_id" value="{{ isset($id) ? $id : '' }}" readonly>
+            </div>
+            <div class="item">
+                <label for="thumbnail">Thumbnail:</label>
+                <input type="file" id="thumbnail" name="thumbnail">
+            </div>
+            <div class="item">
+                <label for="images">Images:</label>
+                <input type="file" id="images" name="images[]" multiple>
+            </div>
+            <div class="item">
+                <label for="make">Make:</label>
+                <input type="text" id="make" name="make" value="{{ isset($data) ? $data['make'] : '' }}">
+            </div>
+            <div class="item">
+                <label for="model">Model:</label>
+                <input type="text" id="model" name="model" value="{{ isset($data) ? $data['model'] : '' }}">
+            </div>
+            <div class="item">
+                <label for="year">Year:</label>
+                <input type="text" id="year" name="year" value="{{ isset($data) ? $data['year'] : '' }}">
+            </div>
+            <div class="item">
+                <label for="chassis">Chassis:</label>
+                <input type="text" id="chassis" name="chassis" value="{{ isset($data) ? $data['chassis'] : '' }}">
+            </div>
+            <div class="item">
+                <label for="body_type">Body Type:</label>
+                <select name="body_type" id="body_type">
+                    @if (isset($data))
+                        <option value="{{ $data['body_type'] }}" selected>{{ $data['body_type'] }}</option>
+                    @else
+                        <option value="" disabled selected>Select Body Type</option>
+                    @endif
+                    <option value="hatchback">Hatchback</option>
+                    <option value="sedan">Sedan</option>
+                    <option value="truck">Truck</option>
+                    <option value="suv">SUV</option>
+                    <option value="van">Van</option>
+                    <option value="pickup">Pickup</option>
+                    <option value="wagon">Wagon</option>
+                    <option value="buses">Buses</option>
+                    <option value="mini buses">Mini Buses</option>
+                </select>
+            </div>
+            <div class="item">
+                <label for="fuel">Fuel:</label>
+                <select name="fuel" id="fuel">
+                    @if (isset($data))
+                        <option value="{{ $data['fuel'] }}" selected>{{ $data['fuel'] }}</option>
+                    @else
+                        <option value="" disabled selected>Select Fuel</option>
+                    @endif
+                    <option value="petrol">Petrol</option>
+                    <option value="diesel">Diesel</option>
+                    <option value="hybrid">Hybrid</option>
+                </select>
+            </div>
+            <div class="item">
+                <label for="mileage">Mileage:</label>
+                <input type="text" id="mileage" name="mileage" value="{{ isset($data) ? $data['mileage'] : '' }}">
+            </div>
+            <div class="item">
+                <label for="transmission">Transmission:</label>
+                <select name="transmission" id="transmission">
+                    @if (isset($data))
+                        <option value="{{ $data['transmission'] }}" selected>{{ $data['transmission'] }}</option>
+                    @else
+                        <option value="" disabled selected>Select Transmission</option>
+                    @endif
+                    <option value="manual">Manual</option>
+                    <option value="automatic">Automatic</option>
+                </select>
+            </div>
+            <div class="item">
+                <label for="doors">Doors:</label>
+                <input type="text" id="doors" name="doors" value="{{ isset($data) ? $data['doors'] : '' }}">
+            </div>
+            <div class="item">
+                <label for="country">Country:</label>
+                <select name="country" id="country">
+                    @if (isset($data))
+                        <option value="{{ $data['country'] }}" selected>{{ $data['country'] }}</option>
+                    @else
+                        <option value="" disabled selected>Select Country</option>
+                    @endif
+                    <option value="jamaica">Jamaica</option>
+                    <option value="bahamas">Bahamas</option>
+                    <option value="guyana">Guyana</option>
+                    <option value="barbados">Barbados</option>
+                    <option value="kenya">Kenya</option>
+                    <option value="tanzania">Tanzania</option>
+                    <option value="ireland">Ireland</option>
+                    <option value="uk">UK</option>
+                    <option value="pakistan">Pakistan</option>
+                    <option value="mauritius">Mauritius</option>
+                </select>
+            </div>
+            <div class="item">
+                <label for="fob">Fob:</label>
+                <input type="text" id="fob" name="fob" value="{{ isset($data) ? $data['fob'] : '' }}">
+            </div>
+            <div class="item">
+                <label for="category">Category:</label>
+                <select name="category" id="category">
+                    @if (isset($data))
+                        <option value="{{ $data['category'] }}" selected>{{ $data['category'] }}</option>
+                    @else
+                        <option value="" disabled selected>Select Category</option>
+                    @endif
+                    <option value="stock">Stock</option>
+                    <option value="new arrival">New Arrival</option>
+                    <option value="discounted">Discounted</option>
+                    <option value="commercial">Commercial</option>
+                </select>
+            </div>
+            <div class="item">
+                <label for="status">Status:</label>
+                <input type="text" id="status" name="status" value="{{ isset($data) ? $data['status'] : '' }}">
+            </div>
+            <div class="item" style="align-items: baseline">
+                <label for="currency">Currency:</label>
+                <select name="currency" id="currency">
+                    @if (isset($data))
+                        <option value="{{ $data['currency'] }}" selected>{{ $data['currency'] }}</option>
+                    @else
+                        <option value="" disabled selected>Select Currency</option>
+                    @endif
+                    <option value="¥">¥</option>
+                    <option value="£">£</option>
+                    <option value="€">€</option>
+                    <option value="$">$</option>
+                </select>
+            </div>
+            <div class="item features" style="align-items: baseline">
+                <label for="features">Features:</label>
+                @if (isset($data))
+                    <div class="content">
+                        <trix-toolbar id="my_toolbar"></trix-toolbar>
+                        <trix-editor toolbar="my_toolbar" input="features">{!! $data['features'] !!}</trix-editor>
+                    </div>
+                @else
+                    <div class="content">
+                        <trix-toolbar id="my_toolbar" class="ql-toolbar"></trix-toolbar>
+                        <trix-editor toolbar="my_toolbar" input="features" id="editor"></trix-editor>
+                    </div>
+                @endif
+            </div>
+            <input type="hidden" id="features" name="features" />
+            <button class="primary">Add</button>
+        </form>
+    </div>
+    @if ($actionUrl == '/stocks/add/store')
+        <script>
+            const thumbnailInput = document.querySelector('#thumbnail');
+            const imagesInput = document.querySelector('#images');
+
+            FilePond.create(thumbnailInput, {
+                allowMultiple: false,
+                server: {
+                    url: '/stocks/add/store',
+                    process: {
+                        headers: {
+                            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+                        }
+                    },
+                },
+            });
+
+            FilePond.create(imagesInput, {
+                allowMultiple: true,
+                server: {
+                    url: '/stocks/add/store',
+                    process: {
+                        headers: {
+                            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content,
+                        }
+                    },
+                },
+            });
+        </script>
+    @endif
+@endsection
