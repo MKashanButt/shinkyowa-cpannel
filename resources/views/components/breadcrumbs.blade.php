5ca65568f78b31bd10edfcd4170bc7b5930135e9
MKashanButt <kashanbutt566@gmail.com>
2025-01-23 20:13:18 +0500

    removed commits

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -8,19 +8,11 @@
         {{ $page }}
     </p>
     @isset($subpage)
-        {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="icon">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-        </svg> --}}
         <a href="{{ redirect()->back() }}">
             <p>{{ $subpage }}</p>
         </a>
     @endisset
     @isset($category)
-        {{-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="icon">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-        </svg> --}}
         <p class="last-url">{{ $category }}</p>
     @endisset
 </div>
