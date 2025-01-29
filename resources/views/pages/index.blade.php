aa214192c4b272e53611d7ed353aaa2ecf0e2503
MKashanButt <kashanbutt566@gmail.com>
2025-01-30 00:14:38 +0500

    added class on counter item heading

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -107,8 +107,9 @@
                     <p>Last Uploaded TT</p>
                 </div>
                 <div class="item">
-                    <h1><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
+                    <h1 class="lastCustomerAdded"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="icon">
                             <circle cx="12" cy="8" r="5" />
                             <path d="M20 21a8 8 0 0 0-16 0" />
                         </svg>{{ $data['lastCustomerAdded'] ? $data['lastCustomerAdded'] : 0 }}</h1>
