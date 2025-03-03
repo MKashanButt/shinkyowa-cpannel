1af23ca6e1f080dac525860183353e4110ad2cef
MKashanButt <kashanbutt566@gmail.com>
2025-03-04 00:25:15 +0500

    added search for stockId

--- resources/views/pages/stocks/index.blade.php
+++ resources/views/pages/stocks/index.blade.php
@@ -26,6 +26,25 @@
                 </button>
             </a>
         @endif
+        <div class="searchForm">
+            <h1>Search Stock Id:</h1>
+            <form action="/search/stockid" method="POST">
+                @csrf
+                @method('POST')
+                <input type="search" name="searchByStockId" id="search" placeholder="Search By Stock Id"
+                    value="{{ Request::get('searchByStockId') ? Request::get('searchByStockId') : '' }}">
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 512 512">
+                        <path d="M256 80a176 176 0 10176 176A176 176 0 00256 80z" fill="none" stroke="currentColor"
+                            stroke-miterlimit="10" stroke-width="32" />
+                        <path d="M232 160a72 72 0 1072 72 72 72 0 00-72-72z" fill="none" stroke="currentColor"
+                            stroke-miterlimit="10" stroke-width="32" />
+                        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10"
+                            stroke-width="32" d="M283.64 283.64L336 336" />
+                    </svg>
+                </button>
+            </form>
+        </div>
         <div class="container">
             <table cellspacing="0">
                 <thead>
