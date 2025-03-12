f9108611a1ad630f272b91f88fdb373490549a27
MKashanButt <kashanbutt566@gmail.com>
2025-03-12 23:45:56 +0500

    minor rem: removed un-used import

--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Storage;
 
 class StocksController extends Controller
