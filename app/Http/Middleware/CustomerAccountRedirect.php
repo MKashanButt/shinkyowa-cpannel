0aac7f2d1c4bb5c704a8eaf1bb6c574c51b61625
MKashanButt <kashanbutt566@gmail.com>
2025-08-07 16:02:37 -0400

    customer account middleware add

---
+++ app/Http/Middleware/CustomerAccountRedirect.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\CustomerAccounts;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomerAccountRedirect
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user()->role == 'customer') {
+            $cid = CustomerAccounts::where('customer_email', Auth::user()->email)->first()->customer_id;
+            return redirect()->route('find-customer-account', ['id' => $cid]);
+        }
+        return $next($request);
+    }
+}
